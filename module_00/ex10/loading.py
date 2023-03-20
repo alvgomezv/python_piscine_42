607283a3c51b57bc09f64a20196f6e443c7d269f
alvgomezv <alkitegv@gmail.com>
2023-03-20 16:30:28 +0100

    Module_00, ex10 completed, Module_00 finished

---
+++ module_00/ex10/loading.py
@@ -0,0 +1,64 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    loading.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alvgomez <alvgomez@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/03/20 12:39:07 by alvgomez          #+#    #+#              #
+#    Updated: 2023/03/20 16:29:20 by alvgomez         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from tqdm import tqdm
+import time
+
+bar = {
+	0  : "[                    ]",
+	1  : "[>                   ]",
+	2  : "[=>                  ]",
+	3  : "[==>                 ]",
+	4  : "[===>                ]",
+	5  : "[====>               ]",
+	6  : "[=====>              ]",
+	7  : "[======>             ]",
+	8  : "[=======>            ]",
+	9  : "[========>           ]",
+	10 : "[=========>          ]",
+	11 : "[==========>         ]",
+	12 : "[===========>        ]",
+	13 : "[============>       ]",
+	14 : "[=============>      ]",
+	15 : "[==============>     ]",
+	16 : "[===============>    ]",
+	17 : "[================>   ]",
+	18 : "[=================>  ]",
+	19 : "[==================> ]",
+	20 : "[===================>]",
+	21 : "[====================]",
+}
+
+def ft_progress(listy):
+	numbers = list(listy)
+	n = len(numbers)
+	t_start = time.time()
+	eta = 0
+	i = 0
+	while i <= numbers[n - 1]:
+		level = int(numbers[i]*20/n)
+		print (f" ETA: {eta:5.2f}s [{int(numbers[i]*100/n):3d}%] {bar[level]} {numbers[i]:4d}/{n:4d} | elapsed time {time.time() - t_start:5.2f}s", end= "\r")
+		yield numbers[i]
+		if i == 0:							
+			t = time.time() - t_start
+		eta = t * (n - i)
+		i += 1
+	print (f"ETA:  0.00s [100%] {bar[21]} {numbers[i - 1] + 1}/{n} | elapsed time {time.time() - t_start:5.2f}s", end= "\r")
+
+if __name__ == "__main__":
+	listy = range(1000)
+	ret = 0
+	for elem in ft_progress(listy): 
+		ret += (elem + 3) % 5
+		time.sleep(0.01) 
+	print()
+	print(ret)
