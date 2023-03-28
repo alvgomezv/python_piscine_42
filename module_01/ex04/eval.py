0b7a8e81bc5eb05a311b533b04409224bf7ddd07
alvgomezv <alkitegv@gmail.com>
2023-03-28 13:45:41 +0200

    Module_01, ex04 completed

---
+++ module_01/ex04/eval.py
@@ -0,0 +1,37 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    eval.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alvgomez <alvgomez@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/03/28 13:23:00 by alvgomez          #+#    #+#              #
+#    Updated: 2023/03/28 13:45:09 by alvgomez         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+class evaluator:
+    def zip_evaluate(coefs, words):
+        if isinstance(coefs, list) and isinstance(words, list) and len(coefs) == len(words):
+            result = 0
+            zipped = zip(coefs, words)
+            for item in zipped:
+                result += item[0] * float(len(item[1]))
+            return result
+        else:
+            return -1
+    def enumerate_evaluate(coefs, words):
+        if isinstance(coefs, list) and isinstance(words, list) and len(coefs) == len(words):
+            result = 0
+            enum1 = enumerate(words)
+            for index, value in enum1:
+                result += coefs[index] * float(len(value))
+            return result
+        else:
+            return -1
+
+if __name__ == "__main__":
+    words = ["Le", "Lorem", "Ipsum", "est", "simple"]
+    coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+    print(evaluator.zip_evaluate(coefs, words))
+    print(evaluator.enumerate_evaluate(coefs, words))
